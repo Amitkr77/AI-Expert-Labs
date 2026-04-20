08f00f0cc9ea8e3f6b8bf8bb46e118995dea359b
pawan282 <pawan@homeasy.io>
2026-04-20 12:14:36 +0530

    logo.png added

--- src/App.tsx
+++ src/App.tsx
@@ -69,7 +69,7 @@ const Navbar = () => {
   return (
     <>
     <nav
-      className={`fixed top-0 left-0 right-0 z-50 py-4 transition-all duration-300
+      className={`fixed top-0 left-0 right-0 z-50 py-0 transition-all duration-300
       ${
         scrolled
           ? "bg-white shadow-md border-b border-slate-200"
